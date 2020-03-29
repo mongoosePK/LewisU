164b6ad2bc919716bcf48db3249af66d7e9a19d0
mongoose <wpulkownik@gmail.com>
2020-03-28 22:37:02 -0500

    add twilio feature

--- spring20/sharefest/sharefestSMS/sharefest/sms/views.py
+++ spring20/sharefest/sharefestSMS/sharefest/sms/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 import datetime
+from twilio.rest import Client
+
 
 @login_required
 def index(request):
@@ -22,6 +24,15 @@ def sign_up(request):
     return render(request, 'sign_up.html',{'form':form})
 
 def compose(request):
+    account_sid = 'AC14a104141d74e903b309af874c4ff626'
+    auth_token = 'bb7db7f8a88d759e5063378e0874f51d'
+    client = Client(account_sid, auth_token)
+    message_to_broadcast = ("Get that Bread")
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient,from_=settings.TWILIO_NUMBER,
+            body=message_to_broadcast)
+    print(message.sid)
     return render(request, 'compose.html')
 
 def upload(request):
